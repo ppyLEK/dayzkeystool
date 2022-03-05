19bef6beb0514193039a55589d6cd5d47b2108ee
Sixtiii <100446318+ppyLEK@users.noreply.github.com>
2022-03-05 08:53:43 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,22 +8,22 @@ using System.IO;
         {
 
             string execPath = AppDomain.CurrentDomain.BaseDirectory;
-            //var checkEmpty = Directory.GetFiles(execPath + destinationFolder, "*.bikey");
-
-            var filteredDirs = Directory.GetFiles(execPath, "*.bikey", SearchOption.AllDirectories);
-            foreach (string s in filteredDirs)
+            var files = Directory.GetFiles(execPath, "*.bikey", SearchOption.AllDirectories);
+            foreach (string s in files)
             {
                 
                 string fileName = Path.GetFileName(s);
 
                 string destFile = Path.Combine(execPath + destinationFolder, fileName);
-                if(destFile != s)
-                {
-                    Console.WriteLine("Moved " + fileName + " to keys folder.");
-                    File.Copy(s, destFile, true);
-                }
-                else
-                    Console.WriteLine(fileName + "| Key is already in keys folder.");
+            if (destFile != s)
+            {
+                Console.WriteLine("Moved " + fileName + " to keys folder.");
+                File.Copy(s, destFile, true);
+            }
+            else
+            {
+                Console.WriteLine(fileName + "| Key is already in keys folder.");
+            }
             }
         }
     }
